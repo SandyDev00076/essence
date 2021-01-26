081a0ca1fae25cbde3b97c998d479750f891a453
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-01-26 23:08:29 +0530

    add neumorphism styles

---
+++ src/components/BPInputs/BPInputs.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BPInputs.module.scss";
+
+const BPInputs = () => {
+  return <section className={styles.container}>cbhbcsjb</section>;
+};
+
+export default BPInputs;
